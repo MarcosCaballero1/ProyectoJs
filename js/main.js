4dc927d95d619b6e121d5197052e6d68c74b1958
Marcos Caballero <caballeromarcos490@gmail.com>
2023-09-12 20:04:15 -0300

    primera entrega js

---
+++ js/main.js
@@ -0,0 +1,64 @@
+
+credito();
+
+function credito() {
+    let nombre = validarNumOString(prompt('nombre'));
+    let edad = validarNumOString(prompt('edad'), 'num');
+    let ciudadania = validarNumOString(prompt('ciudadania'));
+    let montoSolicitado = validarNumOString(prompt('Ingresa monto a solicitar'), 'num');
+    let ingresosMensuales = validarNumOString(prompt('Ingresos mensuales'), 'num');
+    
+    let empleado = yesOrNo(prompt('Ha estado empleado en los ultimos 6 meses? Si/No'));
+    if (empleado.toLowerCase() == 'no') {
+        return alert('Lo siento, para poder solicitar un crédito en nuestro banco se necesita un historial laboral de minimo 6 meses anteriores a hoy');
+    }
+    
+    let bienes = yesOrNo(prompt('bienes a su nombre - Si/No'))
+    if (bienes.toLowerCase() == 'si') {
+        let cantBienes = validarNumOString(prompt('Cuantos bienes tiene a su nombre?'), 'num')
+    }
+
+    let deudasExistentes = yesOrNo(prompt('Cuenta con una deuda existente? Si/No'))
+    if  (deudasExistentes.toLowerCase() == 'si'){
+        let deudaTotal = validarNumOString(prompt('De cuanto es su deuda actual?'), 'num')
+        if (deudaTotal > ingresosMensuales) {
+            return alert('Lo siento, dado a que su deuda es mayor a su salario no puede ser un candidato para obtener un crédito')
+        }
+    }    
+        alert('Aguarde un momento, los funcionarios están corroborando su información para poder aprobar/desaprobar su crédito')
+        sleep(5000).then(() => { 
+            alert('Felicidades, su crédito ha sido aprobado! Solo necesitamos unos datos más y ya estará listo!')
+            let cuotas = validarNumOString(prompt('En cuantas cuotas desea realizar el préstamo?'), 'num')
+            let cantPorMes = montoSolicitado / parseFloat(cuotas);
+            resume(cantPorMes, nombre, montoSolicitado, cuotas, edad)
+        });
+        
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function resume(cantPorMes, nombre, montoSolicitado, cuotas){
+        alert('Felicidades ' + nombre + ' su crédito de ' + montoSolicitado + ' ha sido aprobado con éxito. \n Se deberá abonar ' + cantPorMes + ' por ' + cuotas + ' meses');
+    }
+
+    function validarNumOString(value, numOrNot){
+        if(numOrNot == 'num'){
+            while (isNaN(value)) {
+                value = prompt(value + ' no es numero, por favor, vuelve a intentarlo')
+            }
+        }else{
+            while (!isNaN(value)) {
+                value = prompt('No se permiten números, por favor, vuelve a intentarlo')
+            }
+        }
+        return value;
+    }
+
+    function yesOrNo(siONo) {
+        while(siONo.toLowerCase() != 'no' && siONo.toLowerCase() != 'si') {
+            siONo = prompt('Debe ingresar únicamente Si/No')
+        }
+        return siONo;
+    }
